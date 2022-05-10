30bf8c3ad52b13454a783ca6130e2c45209c0c01
Christian Nytra <christian.nytra@co-orga.d>
2022-05-10 11:16:56 +0200

    ok?!

---
+++ Frontend/Frontend.Module.Web/Controllers/Cudl/GegessenesCudlController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoOrga.XAF.Modules.CoreExtensions.Controllers;
+using CoOrga.XAF.Modules.CoreExtensions.Extensions;
+using NyTEC.EnergyTrecker.Domain.Entities;
+
+namespace Frontend.Module.Web.Controllers.Cudl
+{
+    public class GegessenesCudlController : DisableCudlActionsController<Gegessenes>
+    {
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+
+            this.SetNewActionStatus(Frame, true);
+            this.SetUnlinkActionStatus(Frame, true);
+        }
+    }
+}
